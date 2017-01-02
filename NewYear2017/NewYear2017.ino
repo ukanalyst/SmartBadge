cfcbd0cec32831c691ee8b37f1f5c8c849bb00c3
Dave Almond <gitdev@msg.me.uk>
2017-01-02 12:05:37 +0000

    Adds Rainbow effect and string parsing

--- NewYear2017/NewYear2017.ino
+++ NewYear2017/NewYear2017.ino
@@ -31,42 +31,43 @@ boolean cover[NUM_LEDS];  // array showing which LEDs have been visited
 #define SW4 10
 
 char chargen[][21] = {
-{"01110001110000110110"}, // 0
-{"11100000100001001001"}, // 1
-{"21110011100000110000"}, // 2
-{"31110001110000110001"}, // 3
-{"41000101010000111110"}, // 4
-{"51100111100110100101"}, // 5
-{"61100111110110100101"}, // 6
-{"71100000100011001001"}, // 7
-{"81110111110110100101"}, // 8
-{"91110111100110100101"}, // 9
-{"A1110001010000110111"}, // A
-{"B1110001110000110111"}, // B
-{"C1110001110000000110"}, // C
-{"D1111111100010000100"}, // D
-{"E1100000110000000111"}, // E
-{"F1110000010000010111"}, // F
-{"G1110101111110010001"}, // G
-{"H1010001010000110111"}, // H
-{"I1110001110001001001"}, // I
-{"J0100000111101001001"}, // J
-{"K1100000010001010111"}, // K
-{"L1000001110000000110"}, // L
-{"M1011110001111000001"}, // M
-{"N1000000001111001001"}, // N
-{"O1110001110000110110"}, // O
-{"P1110000010000100111"}, // P
-{"Q1110011110000110110"}, // Q
-{"R1110001010000101111"}, // R
-{"S1111001111000010011"}, // S
-{"T1000000100001101001"}, // T
-{"U1010001110000110110"}, // U
-{"V1010001110000110110"}, // V
-{"W0101111111111001001"}, // W
-{"X0100010100011011011"}, // X
-{"Y1010000110000101011"}, // Y
-{"Z1110011110010100101"} // Z
+  {" 0000000000000000000"}, // <SPACE>
+  {"01110001110000110110"}, // 0
+  {"11100000100001001001"}, // 1
+  {"21110011100000110000"}, // 2
+  {"31110001110000110001"}, // 3
+  {"41000101010000111110"}, // 4
+  {"51100111100110100101"}, // 5
+  {"61100111110110100101"}, // 6
+  {"71100000100011001001"}, // 7
+  {"81110111110110100101"}, // 8
+  {"91110111100110100101"}, // 9
+  {"A1110001010000110111"}, // A
+  {"B1110001110000110111"}, // B
+  {"C1110001110000000110"}, // C
+  {"D1111111100010000100"}, // D
+  {"E1100000110000000111"}, // E
+  {"F1110000010000010111"}, // F
+  {"G1110101111110010001"}, // G
+  {"H1010001010000110111"}, // H
+  {"I1110001110001001001"}, // I
+  {"J0100000111101001001"}, // J
+  {"K1100000010001010111"}, // K
+  {"L1000001110000000110"}, // L
+  {"M1011110001111000001"}, // M
+  {"N1000000001111001001"}, // N
+  {"O1110001110000110110"}, // O
+  {"P1110000010000100111"}, // P
+  {"Q1110011110000110110"}, // Q
+  {"R1110001010000101111"}, // R
+  {"S1111001111000010011"}, // S
+  {"T1000000100001101001"}, // T
+  {"U1010001110000110110"}, // U
+  {"V1010001110000110110"}, // V
+  {"W0101111111111001001"}, // W
+  {"X0100010100011011011"}, // X
+  {"Y1010000110000101011"}, // Y
+  {"Z1110011110010100101"} // Z
 };
 
 // Initial state of buttons. HIGH is OFF, LOW is ON
@@ -79,8 +80,6 @@ int last4 = HIGH;
 void setup() {
   // sanity check delay - allows reprogramming if accidently blowing power w/leds
   delay(2000);
-  Serial.begin(9600);
-  
   pinMode(SW1, INPUT);
   digitalWrite(SW1, HIGH);    // pullup resistors required for switches
   pinMode(SW2, INPUT);
@@ -106,45 +105,20 @@ void setup() {
     delay(50);
     allOff();
   }
-/*  for (int i = 0; i < NUM_LEDS; i++)
-  {
-    leds[i] = CRGB(255, 0, 0);
-    FastLED.show();
-    delay(20);
-  }
-  delay(1000);
-  allOff();*/
+  /*  for (int i = 0; i < NUM_LEDS; i++)
+    {
+      leds[i] = CRGB(255, 0, 0);
+      FastLED.show();
+      delay(20);
+    }
+    delay(1000);
+    allOff();*/
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   if (testButton1()) {
-    Serial.println("Button 1");
-    displaySymbol('H', CRGB::Blue);
-    delay(1000);
-    allOff();
-    displaySymbol('A', CRGB::Blue);
-    delay(1000);
-    allOff();
-    displaySymbol('P', CRGB::Blue);
-    delay(1000);
-    allOff();
-    displaySymbol('P', CRGB::Blue);
-    delay(1000);
-    allOff();
-    displaySymbol('Y', CRGB::Blue);
-    delay(1000);
-    allOff();
-    displaySymbol('2', CRGB::Blue);
-    delay(1000);
-    allOff();
-    displaySymbol('0', CRGB::BlueViolet);
-    delay(1000);
-    allOff();
-    displaySymbol('1', CRGB::Chartreuse);
-    delay(1000);
-    allOff();
-    displaySymbol('7', CRGB::DarkOrange);
+    displayMessage("HAPPY NEW YEAR 2017", false, CRGB::Blue, 500);
     delay(1000);
     allOff();
   } else if (testButton2()) {
@@ -157,35 +131,53 @@ void loop() {
     }
     FastLED.show();
   } else if (testButton3()) {
+    displayMessage("HAPPY NEW YEAR 2017", true, CRGB::Blue, 500);
+    delay(1000);
+    allOff();
   }
 }
 
-void displaySymbol(char symbol, CRGB col)
+
+void displaySymbol(char symbol, boolean rainbow, CRGB col)
 {
-  Serial.println("in DS");
   for (int s = 0; s < (sizeof(chargen) / sizeof(chargen[0])); s++) {
-    Serial.print("array element: ");
-    Serial.println(s);
-    Serial.println(chargen[s][0]);
     if (chargen[s][0] == symbol) {
-      for (int i = 0; i < NUM_LEDS +1; i++)
+      if (rainbow) {
+        for (int c = 0; c <= 360; c++) {     
+          for (int i = 0; i <= NUM_LEDS; i++)
+          {
+            if (chargen[s][i] == '1') {
+              leds[i - 1] = CHSV(c, 255, 255);
+            } else {
+              leds[i - 1] = 0;
+            }
+          }
+          FastLED.show();
+          delay(2);
+        }
+    } else {
+      for (int i = 0; i <= NUM_LEDS; i++)
       {
         if (chargen[s][i] == '1') {
-          Serial.print("Setting LED: ");
-          Serial.print(i);
-          Serial.println(" ON");
-          leds[i-1] = col;
+          leds[i - 1] = col;
         } else {
-          leds[i-1] = 0;
-          Serial.print("Setting LED: ");
-          Serial.print(i);
-          Serial.println(" OFF");
+          leds[i - 1] = 0;
         }
       }
       FastLED.show();
     }
   }
 }
+}
+
+void displayMessage(String message, boolean rainbow, CRGB col, int del)
+{
+  for (int i = 0; message[i] != '\0'; ++i) {
+    displaySymbol(message[i], rainbow, col);
+    delay(del);
+  }
+}
+
 
 boolean testButton1()
 {
